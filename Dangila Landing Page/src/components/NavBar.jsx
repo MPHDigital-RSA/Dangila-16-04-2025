c5d2ee873807e19ffa87c89e1e1394683a6128e9
Prince Hlela <mphdigital@protonmail.com>
2025-04-16 14:34:44 +0200

    created the NavBar.jsx and NavBar.css and styled the navbar using CSS

---
+++ Dangila Landing Page/src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import logo from '../assets/Logo.svg'
+import menu from '../assets/Menu.svg'
+import search from '../assets/Search.svg'
+import cart from '../assets/Basket.svg'
+import profile from '../assets/Profile.svg'
+
+import '../styles/NavBar.css'
+
+function NavBar() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isSearcOpen, setIsSearcOpen] = useState(false);
+
+
+    return (
+        <header className='wrapper header'>
+            <nav className="nav">
+                <img src={logo} alt="this is the Dangila logo" width='125' height='28' className='logo' />
+
+
+                <ul className="desktop-nav">
+                    <li><a href="#">products</a></li>
+                    <li><a href="#">story</a></li>
+                    <li><a href="#">manufacturing</a></li>
+                    <li><a href="#">packaging</a></li>
+                    <li><a href="#">team</a></li>
+                </ul>
+
+                <div className="search-cart-profile">
+                    <img src={search} alt="search icon" width='18.75' height='18.75' />
+                    <img src={cart} alt="cart icon" width='19.69' height='18.98' />
+                    <img src={profile} alt="profile icon" width='17.5' height='19.75' />
+                </div>
+
+                <img src={menu} alt="menu icon" width='37.5' height='21.88' className='menu-icon' />
+
+
+
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
